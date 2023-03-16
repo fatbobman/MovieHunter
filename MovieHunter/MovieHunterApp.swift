e5fc9af5ca785935f992b528113dd0460d882694
fatbobman <xuyang@me.com>
2023-03-16 13:35:55 +0800

    Initial Commit

---
+++ MovieHunter/MovieHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHunterApp.swift
+//  MovieHunter
+//
+//  Created by Yang Xu on 2023/3/16.
+//
+
+import SwiftUI
+
+@main
+struct MovieHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

67e3899e140be9268ad4500b3319f9775b9167fb
fatbobman <xuyang@me.com>
2023-03-21 11:38:06 +0800

    remove debug info

---
+++ MovieHunter/Views/Category/GenreContainer.swift
@@ -0,0 +1,11 @@
+//
+//  GenreContainer.swift
+//  MovieHunter
+//
+//  Created by Yang Xu on 2023/3/21.
+//
+
+import Foundation
+import SwiftUI
+
+
---
+++ MovieHunter/Views/Category/GenreScrollView.swift
@@ -0,0 +1,58 @@
+//
+//  GenreScrollView.swift
+//  MovieHunter
+//
+//  Created by Yang Xu on 2023/3/21.
+//
+
+import Foundation
+import SwiftUI
+import TMDb
+
+struct GenreScrollView: View {
+    let gerneID: Int
+    let inWishlist: (Int) -> Bool
+    let goDetail: (Movie) -> Void
+    let updateWishlist: (Int) -> Void
+    @State private var movies = [Movie]()
+    @Environment(\.tmdb) private var tmdb
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
+                ForEach(movies) { movie in
+                    MovieItemWrapper(
+                        displayType: .portrait(.small),
+                        movie: movie,
+                        goDetail: goDetail
+                    )
+                }
+            }
+            .padding(.bottom, 22)
+        }
+        .scrollContentBackground(.hidden)
+        .safeAreaInset(edge: .leading) {
+            Color.clear
+                .frame(width: 6)
+        }
+        .safeAreaInset(edge: .trailing) {
+            Color.clear
+                .frame(width: 6)
+        }
+        .task {
+            if let result = try? await tmdb.discover.movies(sortedBy: nil, withPeople: nil, withGenres: [gerneID], page: 1) {
+                movies = Array(result.results.prefix(10))
+            }
+        }
+    }
+}
+
+struct GenreScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenreScrollView(
+            gerneID: 12,
+            inWishlist: { _ in true },
+            goDetail: { print($0) },
+            updateWishlist: { print($0) }
+        )
+    }
+}

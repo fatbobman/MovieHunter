54cd3b2647a64c183a041cc02bd309dcdeba790d
fatbobman <xuyang@me.com>
2023-03-24 13:37:12 +0800

    rename

--- MovieHunter/Views/MovieGallery/MovieGalleryDataSource.swift
+++ MovieHunter/Views/MovieGallery/MovieGalleryDataSource.swift
@@ -1,64 +1,83 @@
 //
-//  MovieGalleryDataSource.swift
+//  MovieGalleryContainer.swift
 //  MovieHunter
 //
-//  Created by Yang Xu on 2023/3/24.
+//  Created by Yang Xu on 2023/3/23.
 //
 
 import Foundation
 import SwiftUI
 import TMDb
 
-//struct DataSourceForGallery: DynamicProperty {
-//    @Environment(\.tmdb) private var tmdb
-//    @StateObject private var loader: MoviesGalleryLoader
-//    @FetchRequest
-//    private var favoriteMovies: FetchedResults<FavoriteMovie>
-////    @FetchRequest
-////    private var favoritePersons: FetchedResults<FavoritePerson>
-//    private let source: Source
-//
-//    var movies:some RandomAccessCollection<Movie>{
-//        switch source {
-//        case .tmdb(let category):
-//            return loader
-//        case .wishlist:
-//            return favoriteMovies
-//        }
-//    }
-//    
-//    init(category: Category) {
-//        switch category {
-////        case .favoritePerson:
-////            _favoritePersons = .init(fetchRequest: FavoritePerson.personRequest)
-////            _favoriteMovies = .init(fetchRequest: FavoriteMovie.disableRequest)
-////            source = .person
-//        case .movieWishlist:
-//            _favoriteMovies = .init(fetchRequest: FavoriteMovie.movieRequest)
-////            _favoritePersons = .init(fetchRequest: FavoritePerson.disableRequest)
-//            source = .wishlist
-//        default:
-////            _favoritePersons = .init(fetchRequest: FavoritePerson.disableRequest)
-//            _favoriteMovies = .init(fetchRequest: FavoriteMovie.disableRequest)
-//            source = .tmdb(category)
-//        }
-//        _loader = .init(wrappedValue: .init())
-//    }
-//
-//    func update() {
-//        switch source {
-//        case .tmdb(let category):
-//            if !loader.loaded {
-//                loader.setLoader(category: category, tmdb: tmdb)
-//            }
-//        default:
-//            break
-//        }
-//    }
-//
-//    enum Source {
-//        case tmdb(Category)
-////        case person
-//        case wishlist
-//    }
-//}
+struct MovieGalleryDataSource: View {
+    let category: Category
+
+    @StateObject private var loader = MoviesGalleryLoader()
+    @Environment(\.tmdb) private var tmdb
+    @FetchRequest
+    private var favoriteMovieIDs: FetchedResults<FavoriteMovie>
+    private var source: Source
+    // movies from wishlist movie ids
+    @State private var wishlistMovies = [Movie]()
+    private var movies: AnyRandomAccessCollection<Movie> {
+        switch source {
+        case .tmdb:
+            return AnyRandomAccessCollection(loader)
+        case .wishlist:
+            return AnyRandomAccessCollection(wishlistMovies)
+        }
+    }
+    
+    init(category: Category) {
+        self.category = category
+        switch category {
+        case .movieWishlist:
+            _favoriteMovieIDs = FetchRequest(fetchRequest: FavoriteMovie.movieRequest)
+            source = .wishlist
+        default:
+            source = .tmdb
+            _favoriteMovieIDs = FetchRequest(fetchRequest: FavoriteMovie.disableRequest)
+        }
+    }
+
+    var body: some View {
+        List {
+            ForEach(movies) { movie in
+                MovieItem(movie: movie, displayType: .landscape)
+            }
+        }
+        .onAppear {
+            switch source {
+            case .tmdb:
+                loader.setLoader(category: category, tmdb: tmdb)
+            case .wishlist:
+                break
+            }
+        }
+        .task(id: favoriteMovieIDs.count) {
+            guard source == .wishlist else { return }
+            wishlistMovies = await Movie.loadWishlistMovieByIDs(tmdb: tmdb, movieIDs: Array(favoriteMovieIDs.map { Int($0.movieID) })) // loadMovies(movieIDs: Array(favoriteMovieIDs.map{Int($0.movieID)}))
+        }
+    }
+}
+
+private extension MovieGalleryDataSource {
+    enum Source {
+        case tmdb
+        case wishlist
+    }
+}
+
+/*
+ // 三种数据源的包装器： Tmdb、wishlist、favoritePerson
+  List
+  Grid
+
+  NavigationStack
+  MovieDetail
+  PersonDetail
+  TabView
+  Settings
+  NavigationSplitView
+
+ */

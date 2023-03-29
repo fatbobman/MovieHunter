f07fa9c558f9f5bab8ea58791536532baf081bed
fatbobman <xuyang@me.com>
2023-03-29 19:34:23 +0800

    update

--- MovieHunter/Views/App/WindowGroupRoot.swift
+++ MovieHunter/Views/App/WindowGroupRoot.swift
@@ -8,24 +8,26 @@
 import Foundation
 import SwiftUI
 
-struct WindowGroupRoot: View {
-    let category: Category?
-    @StateObject var store = Store()
-    var body: some View {
-        ContentView()
-            .environmentObject(store)
-            .task {
-                if let category {
-                    store.send(.setDestination(to: [category.destination]))
+#if os(macOS)
+    struct WindowGroupRoot: View {
+        let category: Category?
+        @StateObject var store = Store()
+        var body: some View {
+            ContentView()
+                .environmentObject(store)
+                .task {
+                    if let category {
+                        store.send(.setDestination(to: [category.destination]))
+                    }
                 }
-            }
-    }
-}
-
-#if os(macOS) && DEBUG
-    struct WindowGroupRoot_Previews: PreviewProvider {
-        static var previews: some View {
-            WindowGroupRoot(category: .popular)
         }
     }
+
+    #if DEBUG
+        struct WindowGroupRoot_Previews: PreviewProvider {
+            static var previews: some View {
+                WindowGroupRoot(category: .popular)
+            }
+        }
+    #endif
 #endif
